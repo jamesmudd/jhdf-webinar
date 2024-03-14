1b7a84e7467383745fb5034808f5c2d75c41eb3b
James Mudd <james.mudd@gmail.com>
2024-03-14 21:10:46 +0000

    Cleanup

--- hdf5-example/weather_to_h5.py
+++ hdf5-example/weather_to_h5.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
+# Data from https://www.kaggle.com/code/timmofeyy/weather-worldwide-eda-visualization/input?select=weather.csv
 daily_weather = pd.read_csv("/home/james/git/jhdf-webinar/hdf5-example/weather.csv")
 
 print(daily_weather.describe())
 
 daily_weather.to_hdf("weather.hdf5", key="weather", mode="w", complevel=8, format="table", data_columns=True)
+print("written [weather.hdf5]")
